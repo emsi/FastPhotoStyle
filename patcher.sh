8fa760f91514bf24932ec42cffca6dd7a615c7ec
Mariusz Woloszyn <emsi>
2018-02-24 18:53:41 +0100

    No need it any more, the patch is in docker image

--- patcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh 
-
-wget  --no-check-certificate https://github.com/NVIDIA/pynvrtc/commit/6417a2896ff8a99f2c4d4195de657671a77c89a0.diff
-cwd=$(pwd)
-cd /anaconda3/lib/python3.6/site-packages/pynvrtc/ 
-patch < ${cwd}/6417a2896ff8a99f2c4d4195de657671a77c89a0.diff 
-cd ${cwd}
